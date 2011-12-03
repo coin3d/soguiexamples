0657003d4fb7a8e225cc14700bc8d1b4c72f67e3
Marius Kintel <marius@kintel.net>
2011-12-03 17:07:43 +0100

    Updated license headers to BSD

--- volumerendering/Cute/curvep/SbGuiCubicSpline.cpp
+++ volumerendering/Cute/curvep/SbGuiCubicSpline.cpp
@@ -1,24 +1,33 @@
 /**************************************************************************\
- *
- *  This file is part of the SmallChange extension library for Coin.
- *  Copyright (C) 1998-2002 by Systems in Motion.  All rights reserved.
- *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU General Public License
- *  version 2 as published by the Free Software Foundation.  See the
- *  file LICENSE.GPL at the root directory of this source distribution
- *  for more details.
- *
- *  If you desire to use SmallChange with software that is incompatible
- *  licensewise with the GPL, and / or you would like to take
- *  advantage of the additional benefits with regard to our support
- *  services, please contact Systems in Motion about acquiring a Coin
- *  Professional Edition License.  See <URL:http://www.coin3d.org> for
- *  more information.
- *
- *  Systems in Motion, Prof Brochs gate 6, 7030 Trondheim, NORWAY
- *  <URL:http://www.sim.no>, <mailto:support@sim.no>
- *
+ * Copyright (c) Kongsberg Oil & Gas Technologies AS
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 \**************************************************************************/
 
 #include <Inventor/Qt/SoQtBasic.h>
